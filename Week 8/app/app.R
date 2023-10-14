147886e6a0c8761d604f9702583a1f92c96cf196
HarizEmran <muhdharizbinemran@gmail.com>
2023-10-14 10:47:10 +0800

    Update (Weeks 1 to 8)
    Upload of all files from Weeks 1 to 8

---
+++ Week 8/app/app.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(datasets)
+
+# Data pre-processing ----
+# Tweak the "am" variable to have nicer factor labels -- since this
+# doesn't rely on any user inputs, we can do this once at startup
+# and then use the value throughout the lifetime of the app
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Challenge 8: Fuel Economy"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for variable to plot against mpg ----
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear",
+                    "Horsepower" = "hp",
+                    "Displacement Size per Cylinder" = "disp")),
+      
+      # Input: Checkbox for whether outliers should be included ----
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Formatted text for caption ----
+      h3(textOutput("caption")),
+      
+      # Output: Plot of the requested variable against mpg ----
+      plotOutput("mpgPlot"),
+      
+      h3("BMW M3 & Honda Civic"),
+      img(src = "car.jpeg", height = 60, width = 100),
+      img(src = "car2.jpeg", height = 60, width = 100),
+      
+      h3("Conclusion of Findings"),
+      p("As the findings above demonstrate, fuel economy often referred to as miles per gallon (mpg) is determined by various factors such as the type of transmission and the gear being used. This",
+        a(span("website", style = "color:blue"), href = "https://mobil.com/en/sap/personal-vehicles/car/vehicle-maintenance/reduce-fuel-consumption"), 
+        "provides tips on how to improve your car's fuel economy when driving."),
+      
+    )
+  )
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+  # Compute the formula text ----
+  # This is in a reactive expression since it is shared by the
+  # output$caption and output$mpgPlot functions
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption ----
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg ----
+  # and only exclude outliers if requested
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()),
+            data = mpgData,
+            outline = input$outliers,
+            col = "#75AADB", pch = 19)
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
